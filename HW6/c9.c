d44f7a31aef8df97ba3ecaf347ded2641420f626
Dmitrii <dr@ritm.studio>
2025-11-16 19:47:46 +0300

    feat: add c9 task solution

--- HW6/c9.c
+++ HW6/c9.c
@@ -1,5 +1,6 @@
 // Факториал
-// Составить функцию вычисления N!. Использовать ее при вычислении факториала int factorial(int n)
+// Составить функцию вычисления N!. Использовать ее при вычислении факториала
+// int factorial(int n)
 
 // Формат входных данных
 // Целое положительное число не больше 20
@@ -10,3 +11,20 @@
 // 5
 // Результат работы
 // 120
+
+#include <stdio.h>
+
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+int main(void) {
+    int input;
+    scanf("%d", &input);
+    printf("%d", factorial(input));
+    return 0;
+}
