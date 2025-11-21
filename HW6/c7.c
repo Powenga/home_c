301d5c326e237e924a62537ee8f568a57a49ae1d
Dmitrii <rakhmanin.dm@gmail.com>
2025-11-21 20:56:28 +0300

    feat: add task 7 solution

--- HW6/c7.c
+++ HW6/c7.c
@@ -1,5 +1,6 @@
 // Перевести число N в систему счисления P
-// Составить функцию, которая переводит число N из десятичной системы счисления в P-ичную систему счисления.
+// Составить функцию, которая переводит число N из десятичной системы счисления
+// в P-ичную систему счисления.
 
 // Формат входных данных
 // Два целых числа. N ≥ 0 и 2 ≤ P ≤ 9
@@ -18,3 +19,31 @@
 // 11 3
 // Результат работы
 // 102
+
+#include <stdio.h>
+
+long long translateDecToAny(int dec, int base) {
+    long long result = 0;
+    int multiplier = 1;
+    while (dec > 0) {
+        result += dec % base * multiplier;
+        dec /= base;
+        multiplier *= 10;
+    }
+    return result;
+}
+
+int main(void) {
+    int dec, base;
+    scanf("%d %d", &dec, &base);
+    if (dec < 0) {
+        printf("Число должно быть не меньше 0");
+        return 0;
+    }
+    if (base < 2 || base > 9) {
+        printf("Основание должно быть не меньше 2 и не больше 9");
+        return 0;
+    }
+    printf("%lld", translateDecToAny(dec, base));
+    return 0;
+}
