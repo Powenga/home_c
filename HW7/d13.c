38bfc7e92d2875f3c0ef68f408b890ac2a579c41
Dmitrii <rakhmanin.dm@gmail.com>
2025-11-27 23:30:38 +0300

    feat: add task d13 solution

--- HW7/d13.c
+++ HW7/d13.c
@@ -14,3 +14,30 @@
 // 120
 // Результат работы
 // 2 2 2 3 5
+
+#include <stdio.h>
+
+void printPrimeDevider(int num, int delitel) {
+    if (delitel > num) {
+        return;
+    }
+    if (num % delitel == 0) {
+        printf("%d ", delitel);
+        num /= delitel;
+    } else {
+        delitel += 1;
+    }
+    printPrimeDevider(num, delitel);
+}
+
+int main() {
+    int input;
+    scanf("%d", &input);
+
+    if (input < 1) {
+        printf("Введите число больше 1!");
+    }
+
+    printPrimeDevider(input, 2);
+    return 0;
+}
