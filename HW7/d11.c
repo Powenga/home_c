f3f5942ca098fc2152b5b5ec8a5482d6fa1516a4
Dmitrii <rakhmanin.dm@gmail.com>
2025-11-27 22:39:41 +0300

    feat: add task d11 solution

--- HW7/d11.c
+++ HW7/d11.c
@@ -14,3 +14,36 @@
 // 255
 // Результат работы
 // 8
+
+#include <stdio.h>
+
+long long translateDecToAny(int num, int base) {
+    if (base < 2 || base > 9) {
+        printf("Основание должно быть больше 1 и меньше 10!");
+        return 0;
+    }
+
+    if (num > 0) {
+        return translateDecToAny(num / base, base) * 10 + (num % base);
+    } else {
+        return 0;
+    }
+}
+
+int summDigits(long long num) {
+    if (num <= 0) {
+        return 0;
+    }
+    return num % 10 + summDigits(num / 10);
+}
+
+int main() {
+    int input, res;
+    scanf("%d", &input);
+    if (input < 0) {
+        printf("Введите не отрицательное число!");
+    }
+
+    printf("%d", summDigits(translateDecToAny(input, 2)));
+    return 0;
+}
