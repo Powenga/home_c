0225953329982f1f15dd8d34b6d58d11bc09fa43
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-10 00:02:21 +0300

    feat: add f1 task solution

---
+++ HW9/f1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define SIZE 20
+
+int printIntArray(int arr[], int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+void sort_array(int size, int a[]) {
+    int noSwap;
+    for (int i = 0; i < size; i++) {
+        noSwap = 1;
+        for (int j = 0; j < size - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                noSwap = 0;
+            }
+        }
+
+        if (noSwap) break;
+    }
+}
+
+int main(void) {
+    int arr[SIZE] = {0};
+    inputIntArray(arr, SIZE);
+    sort_array(arr, SIZE);
+    printIntArray(arr, SIZE);
+    return 0;
+}
