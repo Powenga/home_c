706d69b8ce183525027c79b05c34e2ee218c5113
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-12 01:36:03 +0300

    feat: add more helpers to array-lib

--- array-lib.c
+++ array-lib.c
@@ -18,6 +18,22 @@ void shiftArrayRight(int array[], int size) {
     array[0] = temp;
 }
 
+void shiftArrayLeft(int array[], int size) {
+    int temp = array[0];
+    for (int i = 0; i < size - 1; i++) {
+        array[i] = array[i + 1];
+    }
+    array[size - 1] = temp;
+}
+
+void shiftArrayLeftPartial(int array[], int size, int startIndex) {
+    int temp = array[startIndex];
+    for (int i = startIndex; i < size - 1; i++) {
+        array[i] = array[i + 1];
+    }
+    array[size - 1] = temp;
+}
+
 void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
