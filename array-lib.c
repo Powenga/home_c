24bb0f76836389f5f8dded9a881ead341d6b28fe
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-12 00:27:21 +0300

    feat: add helpers to array-lib

--- array-lib.c
+++ array-lib.c
@@ -9,3 +9,17 @@ int inputIntArray(int arr[], int length) {
         scanf("%d", &arr[i]);
     }
 }
+
+void shiftArrayRight(int array[], int size) {
+    int temp = array[size - 1];
+    for (int i = size - 1; i > 0; i--) {
+        array[i] = array[i - 1];
+    }
+    array[0] = temp;
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
