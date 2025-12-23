93b7d3e0ace929f24230b99091f2fa3603d6ed1a
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-24 00:02:40 +0300

    feat: add year averaget temp function

--- HW11/src/temp_api.c
+++ HW11/src/temp_api.c
@@ -5,6 +5,14 @@
 int8_t get_month_average_temp(temperature_record_t* year, uint8_t month) {}
 int8_t get_month_min_temp(temperature_record_t* year, uint8_t month) {};
 int8_t get_month_max_temp(temperature_record_t* year, uint8_t month) {};
-int8_t get_year_average_temp(temperature_record_t* year) { uint8_t sum = 0; };
+
+int8_t get_year_average_temp(temperature_record_t* year, uint8_t size) {
+    uint8_t sum = 0;
+    for (size_t i = 0; i < size; i++) {
+        sum += year->temperature;
+    }
+    return sum / size;
+};
+
 int8_t get_year_min_temp(temperature_record_t* year) {};
 int8_t get_year_max_temp(temperature_record_t* year) {};
