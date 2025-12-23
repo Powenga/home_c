4a08741bfbc3e620ce4d8dde703d2c73b0d61fea
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-24 00:01:01 +0300

    fix: fix return types

--- HW11/include/temp_api.h
+++ HW11/include/temp_api.h
@@ -5,11 +5,11 @@
 
 #include "temperature_record.h"
 
-uint8_t get_month_average_temp(temperature_record_t* year, uint8_t month);
-uint8_t get_month_min_temp(temperature_record_t* year, uint8_t month);
-uint8_t get_month_max_temp(temperature_record_t* year, uint8_t month);
-uint8_t get_year_average_temp(temperature_record_t* year);
-uint8_t get_year_min_temp(temperature_record_t* year);
-uint8_t get_year_max_temp(temperature_record_t* year);
+int8_t get_month_average_temp(temperature_record_t* year, uint8_t month);
+int8_t get_month_min_temp(temperature_record_t* year, uint8_t month);
+int8_t get_month_max_temp(temperature_record_t* year, uint8_t month);
+int8_t get_year_average_temp(temperature_record_t* year);
+int8_t get_year_min_temp(temperature_record_t* year);
+int8_t get_year_max_temp(temperature_record_t* year);
 
 #endif
