64c10e80677ed0b81beb535137e8f1f144591e52
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-24 00:06:44 +0300

    feat: add get min temp function, change struct attibute to array

--- HW11/src/temp_api.c
+++ HW11/src/temp_api.c
@@ -6,13 +6,22 @@ int8_t get_month_average_temp(temperature_record_t* year, uint8_t month) {}
 int8_t get_month_min_temp(temperature_record_t* year, uint8_t month) {};
 int8_t get_month_max_temp(temperature_record_t* year, uint8_t month) {};
 
-int8_t get_year_average_temp(temperature_record_t* year, uint8_t size) {
-    uint8_t sum = 0;
+int8_t get_year_average_temp(temperature_record_t year[], uint8_t size) {
+    int8_t sum = 0;
     for (size_t i = 0; i < size; i++) {
-        sum += year->temperature;
+        sum += year[i].temperature;
     }
     return sum / size;
 };
 
-int8_t get_year_min_temp(temperature_record_t* year) {};
+int8_t get_year_min_temp(temperature_record_t year[], uint8_t size) {
+    int8_t min = year[0].temperature;
+    for (size_t i = 1; i < size; i++) {
+        int8_t current_temp = year[i].temperature;
+        if (current_temp < min) {
+            min = current_temp;
+        }
+    }
+    return min;
+};
 int8_t get_year_max_temp(temperature_record_t* year) {};
