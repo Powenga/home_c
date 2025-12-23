5998490d5ba00d7e8dfa57776ec949f41a5a03dc
Dmitrii <rakhmanin.dm@gmail.com>
2025-12-24 00:12:45 +0300

    feat: add get month average temperature

--- HW11/src/temp_api.c
+++ HW11/src/temp_api.c
@@ -2,7 +2,19 @@
 
 #include "temperature_record.h"
 
-int8_t get_month_average_temp(temperature_record_t* year, uint8_t month) {}
+int8_t get_month_average_temp(temperature_record_t year[], uint8_t size,
+                              uint8_t month) {
+    int8_t month_sum = 0;
+    uint8_t month_count = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (year[i].month == month) {
+            month_sum += year[i].temperature;
+            month_count++;
+        }
+    }
+    return month_sum / month_count;
+}
+
 int8_t get_month_min_temp(temperature_record_t* year, uint8_t month) {};
 int8_t get_month_max_temp(temperature_record_t* year, uint8_t month) {};
 
