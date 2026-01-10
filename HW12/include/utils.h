6a27e0e695754277d691acbb9351fe527dd50148
Dmitrii <rakhmanin.dm@gmail.com>
2026-01-10 23:41:13 +0300

    fix: add record array funtions to header file

--- HW12/include/utils.h
+++ HW12/include/utils.h
@@ -5,6 +5,14 @@
 
 #include "temperature_record.h"
 
+void add_record(temperature_record_t* data, uint16_t position, uint16_t year,
+                uint8_t month, uint8_t day, uint8_t hours, uint8_t minutes,
+                int8_t temperature);
+
+void remove_record(temperature_record_t* data, uint16_t* count, uint16_t index);
+
+void create_temperature_records(temperature_record_t* data, int16_t count);
+
 uint64_t date_to_int(temperature_record_t* record);
 
 #endif
