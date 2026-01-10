8f422eb00cdb3a7ff0905d9085c70e474ab6e105
Dmitrii <rakhmanin.dm@gmail.com>
2026-01-11 02:31:32 +0300

    ref: rename date->records, count->size

--- HW12/src/temp_api.c
+++ HW12/src/temp_api.c
@@ -4,18 +4,18 @@
 #include "temperature_record.h"
 #include "utils.h"
 
-int8_t get_month_average_temp(temperature_record_t* data, uint16_t size,
+int8_t get_month_average_temp(temperature_record_t* records, uint16_t size,
                               uint8_t month, int8_t* p_average_temp) {
     int8_t result = 0;  // true if valid data exists
     int8_t month_sum = 0;
     uint8_t month_count = 0;
     for (uint16_t i = 0; i < size; i++) {
-        if (!data[i].valid) {
+        if (!records[i].valid) {
             continue;
         }
         // Sum and count if it's current month
-        if (data[i].month == month) {
-            month_sum += data[i].temperature;
+        if (records[i].month == month) {
+            month_sum += records[i].temperature;
             month_count++;
             result = 1;  // Some data is valid
         }
@@ -26,16 +26,16 @@ int8_t get_month_average_temp(temperature_record_t* data, uint16_t size,
     return result;
 }
 
-int8_t get_month_min_temp(temperature_record_t* data, uint16_t size,
+int8_t get_month_min_temp(temperature_record_t* records, uint16_t size,
                           uint8_t month, int8_t* p_min_temp) {
     int8_t result = 0;  // true if valid data exists in month
     // Find first
     for (uint16_t i = 0; i < size; i++) {
         // Skip not valid data or other month
-        if (!data[i].valid || data[i].month != month) {
+        if (!records[i].valid || records[i].month != month) {
             continue;
         }
-        int8_t current_temp = data[i].temperature;
+        int8_t current_temp = records[i].temperature;
         // First founded temp;
         if (!result) {
             *p_min_temp = current_temp;
@@ -48,16 +48,16 @@ int8_t get_month_min_temp(temperature_record_t* data, uint16_t size,
     return result;
 };
 
-int8_t get_month_max_temp(temperature_record_t* data, uint16_t size,
+int8_t get_month_max_temp(temperature_record_t* records, uint16_t size,
                           uint8_t month, int8_t* p_max_temp) {
     int8_t result = 0;  // true if valid data exists in month
     // Find first
     for (uint16_t i = 0; i < size; i++) {
         // Skip not valid data or other month
-        if (!data[i].valid || data[i].month != month) {
+        if (!records[i].valid || records[i].month != month) {
             continue;
         }
-        int8_t current_temp = data[i].temperature;
+        int8_t current_temp = records[i].temperature;
         // First founded temp;
         if (!result) {
             *p_max_temp = current_temp;
@@ -70,17 +70,17 @@ int8_t get_month_max_temp(temperature_record_t* data, uint16_t size,
     return result;
 };
 
-int8_t get_year_average_temp(temperature_record_t* data, uint16_t size,
+int8_t get_year_average_temp(temperature_record_t* records, uint16_t size,
                              int8_t* p_average_temp) {
     int8_t result = 0;  // true if valid data exists in year
     int8_t sum = 0;
     uint8_t count = 0;
     for (size_t i = 0; i < size; i++) {
-        if (!data[i].valid) {
+        if (!records[i].valid) {
             continue;
         }
 
-        sum += data[i].temperature;
+        sum += records[i].temperature;
         count++;
         result = 1;
     }
@@ -90,16 +90,16 @@ int8_t get_year_average_temp(temperature_record_t* data, uint16_t size,
     return result;
 };
 
-int8_t get_year_min_temp(temperature_record_t* data, uint16_t size,
+int8_t get_year_min_temp(temperature_record_t* records, uint16_t size,
                          int8_t* p_min_temp) {
     int8_t result = 0;  // true if valid data exists in year
     for (size_t i = 0; i < size; i++) {
         // Skip not valid data
-        if (!data[i].valid) {
+        if (!records[i].valid) {
             continue;
         }
 
-        int8_t current_temp = data[i].temperature;
+        int8_t current_temp = records[i].temperature;
         if (!result) {
             *p_min_temp = current_temp;
             result = 1;
@@ -111,16 +111,16 @@ int8_t get_year_min_temp(temperature_record_t* data, uint16_t size,
     return result;
 };
 
-int8_t get_year_max_temp(temperature_record_t* data, uint16_t size,
+int8_t get_year_max_temp(temperature_record_t* records, uint16_t size,
                          int8_t* p_max_temp) {
     int8_t result = 0;  // true if valid data exists in year
     for (size_t i = 0; i < size; i++) {
         // Skip not valid data
-        if (!data[i].valid) {
+        if (!records[i].valid) {
             continue;
         }
 
-        int8_t current_temp = data[i].temperature;
+        int8_t current_temp = records[i].temperature;
         if (!result) {
             *p_max_temp = current_temp;
             result = 1;
@@ -132,55 +132,55 @@ int8_t get_year_max_temp(temperature_record_t* data, uint16_t size,
     return result;
 };
 
-uint8_t add_record(temperature_record_t* data, uint16_t position, uint16_t year,
-                   uint8_t month, uint8_t day, uint8_t hours, uint8_t minutes,
-                   int8_t temperature) {
+uint8_t add_record(temperature_record_t* records, uint16_t position,
+                   uint16_t year, uint8_t month, uint8_t day, uint8_t hours,
+                   uint8_t minutes, int8_t temperature) {
     uint8_t valid =
         validate_record(year, month, day, hours, minutes, temperature);
     if (!valid) {
         return 0;
     }
-    data[position].year = year;
-    data[position].month = month;
-    data[position].day = day;
-    data[position].hours = hours;
-    data[position].minutes = minutes;
-    data[position].temperature = temperature;
-    data[position].valid = valid;
+    records[position].year = year;
+    records[position].month = month;
+    records[position].day = day;
+    records[position].hours = hours;
+    records[position].minutes = minutes;
+    records[position].temperature = temperature;
+    records[position].valid = valid;
     return 1;
 }
 
-int8_t remove_record(temperature_record_t* data, uint16_t* count,
+int8_t remove_record(temperature_record_t* records, uint16_t* size,
                      uint16_t index) {
-    if (index >= *count) {
+    if (index >= *size) {
         return 0;
     }
 
-    for (uint16_t i = index; i < *count - 1; i++) {
-        data[i] = data[i + 1];
+    for (uint16_t i = index; i < *size - 1; i++) {
+        records[i] = records[i + 1];
     }
-    (*count)--;
+    (*size)--;
     return 1;
 }
 
-uint16_t create_temperature_records(temperature_record_t* data,
-                                    uint16_t count) {
+uint16_t create_temperature_records(temperature_record_t* records,
+                                    uint16_t size) {
     uint16_t year = 1970;
     uint8_t month = 1;
     uint8_t day = 1;
     uint8_t hours = 0;
     uint8_t minutes = 1;
-    for (uint16_t i = 0; i < count; i++) {
+    for (uint16_t i = 0; i < size; i++) {
         int8_t temperature = (int8_t)(rand() % 201 - 100);  // [-100; 100]
-        add_record(data, i, year, month, day, hours, minutes, temperature);
+        add_record(records, i, year, month, day, hours, minutes, temperature);
         increment_day(&year, &month, &day);
     }
-    return count;
+    return size;
 }
 
-static void sort_temperature_records_by_date(temperature_record_t* data,
-                                             uint16_t count) {
-    qsort(data, count, sizeof(temperature_record_t),
+static void sort_temperature_records_by_date(temperature_record_t* records,
+                                             uint16_t size) {
+    qsort(records, size, sizeof(temperature_record_t),
           temp_record_comparator_by_date);
 }
 
@@ -191,25 +191,25 @@ static void print_record(temperature_record_t* record,
            record->minutes, record->temperature);
 };
 
-void print_temperature_records(temperature_record_t* data, uint16_t count) {
-    sort_temperature_records_by_date(data, count);
+void print_temperature_records(temperature_record_t* records, uint16_t size) {
+    sort_temperature_records_by_date(records, size);
     uint16_t increment_position = 1;
-    for (uint16_t i = 0; i < count; i++) {
-        if (!data[i].valid) {
+    for (uint16_t i = 0; i < size; i++) {
+        if (!records[i].valid) {
             continue;
         }
-        print_record(&data[i], increment_position++);
+        print_record(&records[i], increment_position++);
     }
 }
 
-void print_temperature_records_by_month(temperature_record_t* data,
-                                        uint16_t count, uint8_t month) {
-    sort_temperature_records_by_date(data, count);
+void print_temperature_records_by_month(temperature_record_t* records,
+                                        uint16_t size, uint8_t month) {
+    sort_temperature_records_by_date(records, size);
     uint16_t increment_position = 1;
-    for (uint16_t i = 0; i < count; i++) {
-        if (!data[i].valid || data[i].month != month) {
+    for (uint16_t i = 0; i < size; i++) {
+        if (!records[i].valid || records[i].month != month) {
             continue;
         }
-        print_record(&data[i], increment_position++);
+        print_record(&records[i], increment_position++);
     }
 }
