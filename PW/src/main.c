41cbc2f5635e24b9a97164d48be59bfb3c057ee6
Dmitrii <rakhmanin.dm@gmail.com>
2026-01-28 20:07:27 +0300

    feat: add .clang-format

--- PW/src/main.c
+++ PW/src/main.c
@@ -13,53 +13,53 @@
 
 #define DATA_SIZE 256
 
-int main(int argc, char* argv[]) {
-    CliOptions options;
-    if (!parse_cli(argc, argv, &options)) {
-        print_cli_usage(argv[0]);
-        return 0;
-    }
-
-    // Help message
-    if (options.show_help) {
-        print_help(argv[0]);
-        return 0;
-    }
-
-    if (!options.input_file) {
-        printf("Please, provide path to file.\n");
-        return 1;
-    }
-
-    // Init records store
-    Records data;
-    init_temperature_array(&data, DATA_SIZE);
-
-    // Read file and put record
-    FILE* file;
-    file = fopen(options.input_file, "r");
-
-    if (!file) {
-        printf("Can't open file %s.\n", options.input_file);
-        return 1;
-    }
-
-    // load data from file
-    load_records_from_csv(file, &data);
-
-    // Statisctcs
-    printf("\n");
-    FullStatistics full_statistics;
-    init_full_statistics(&full_statistics);
-
-    calculate_temperature_statistics(&data, &full_statistics);
-
-    if (options.month == -1) {
-        print_all_stats(&full_statistics);
-    } else {
-        print_month_stats(&full_statistics, options.month);
-    }
-    printf("\n");
-
+int main(int argc, char *argv[]) {
+  CliOptions options;
+  if (!parse_cli(argc, argv, &options)) {
+    print_cli_usage(argv[0]);
     return 0;
+  }
+
+  // Help message
+  if (options.show_help) {
+    print_help(argv[0]);
+    return 0;
+  }
+
+  if (!options.input_file) {
+    printf("Please, provide path to file.\n");
+    return 1;
+  }
+
+  // Init records store
+  Records data;
+  init_temperature_array(&data, DATA_SIZE);
+
+  // Read file and put record
+  FILE *file;
+  file = fopen(options.input_file, "r");
+
+  if (!file) {
+    printf("Can't open file %s.\n", options.input_file);
+    return 1;
+  }
+
+  // load data from file
+  load_records_from_csv(file, &data);
+
+  // Statisctcs
+  printf("\n");
+  FullStatistics full_statistics;
+  init_full_statistics(&full_statistics);
+
+  calculate_temperature_statistics(&data, &full_statistics);
+
+  if (options.month == -1) {
+    print_all_stats(&full_statistics);
+  } else {
+    print_month_stats(&full_statistics, options.month);
+  }
+  printf("\n");
+
+  return 0;
 }
